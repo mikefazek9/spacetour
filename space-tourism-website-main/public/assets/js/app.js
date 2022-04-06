b1c8e3d2c6915bf891db1ff4b25c1f13c2b4e042
Michael Fazekas <71743477+mikefazek9@users.noreply.github.com>
2022-04-05 21:47:17 -0500

    site 95% completed
    mobile response to home page, destination page, and crew page, fixed bug *feature* with nav bar during mobile viewport.  changed active class in js for crew page.

--- space-tourism-website-main/public/assets/js/app.js
+++ space-tourism-website-main/public/assets/js/app.js
@@ -11,12 +11,29 @@ const toggleNav = () => {
     }
 }
 
+//nav active page
+const navPage = window.location.pathname;
+const navLinks =document.querySelectorAll('.nav-items a').forEach(link =>{
+    if(link.href.includes(`${navPage}`)){
+        link.classList.add('nav-items-active');
+    }
+})
+
+
+//Planet active page
+
+const planetPage = window.location.pathname;
+const planetLinks = document.querySelectorAll('.des-link-cc a').forEach(link =>{
+    if(link.href.includes(`${planetPage}`)){
+        link.classList.add('dest-planet-links-active');
+    }
+})
 
 // ACTIVE STATE LINKS ON CREW
 const activePage = window.location.pathname;
 const crewLinks = document.querySelectorAll('li a').forEach(link =>{
     if(link.href.includes(`${activePage}`)){
-        link.classList.add('active');    
+        link.classList.add('crew-active');    
     }
 })
 // end 
